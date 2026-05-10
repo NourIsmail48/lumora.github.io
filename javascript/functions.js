8ce2156b08b993ba9a5792fa9ca6f7bc450c9117
NourIsmail48 <nourita420@gmail.com>
2026-05-10 23:10:44 +0300

    Update functions.js

--- javascript/functions.js
+++ javascript/functions.js
@@ -106,7 +106,7 @@ function addToWishlistCandles(that, productId) {
   that.setAttribute("onclick", `removeFromWhishlistCandles(this,${productId})`);
 }
 function removeFromWhishlistCandles(that, productId) {
-  wishlist.filter(function (item) {
+  cart.filter(function (item) {
     return item.id != productId;
   });
   updateLocalStorage();
@@ -137,7 +137,7 @@ function addToWishlistDryOil(that, productId) {
   that.setAttribute("onclick", `removeFromWhishlistDryOil(this,${productId})`);
 }
 function removeFromWhishlistDryOil(that, productId) {
-  wishlist.filter(function (item) {
+  cart.filter(function (item) {
     return item.id != productId;
   });
   updateLocalStorage();
